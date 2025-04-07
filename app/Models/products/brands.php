24cabf5097b7992195f29067ca0d1062f3ba265f
santiago <everardoorozcocarmona@gmail.com>
2025-04-07 17:04:05 -0500

    feat: The product models and their related tables were created and configured

---
+++ app/Models/products/brands.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models\products;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class brands extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'pk_brand';
+}
---
+++ app/Models/products/products.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models\products;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class products extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'pk_product';
+
+    public function categories()
+    {
+        return $this->belongsTo(categories::class, 'fk_category', 'pk_category');
+    }
+
+    public function brands()
+    {
+        return $this->belongsTo(brands::class, 'fk_brand', 'pk_brand');
+    }
+
+    public function productVariants()
+    {
+        return $this->hasMany(product_variants::class, 'fk_variant', 'pk_variant');
+    }
+
+    public function productVariant()
+    {
+        return $this->hasOne(product_variants::class, 'fk_variant', 'pk_variant');
+    }
+
+    public function productImages()
+    {
+        return $this->hasMany(product_images::class, 'fk_image', 'pk_image_product');
+    }
+}
